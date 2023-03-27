1afaa94bd0d8a2e55e5ad0ba5e6ebfe3e19f07ff
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-27 14:57:15 +0300

    HM_5 done

---
+++ C_Sharp/Seminar_2_HM/HM_5/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программы, которая принимает на ввод трехзначное число 
+и на выходе показывает только вторую цифру этого числа
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Clear();
+Console.Write("Enter a three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+int a = num % 100;
+int b = a / 10;
+Console.WriteLine($"{num} -> {b}");
