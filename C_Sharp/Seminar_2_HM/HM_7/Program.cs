132a68f7946af0dc63bda5e878438a03846c18bb
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-27 16:00:04 +0300

    HM 7 done

---
+++ C_Sharp/Seminar_2_HM/HM_7/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+Console.Write("Enter day's number of week: ");
+int a = int.Parse(Console.ReadLine());
+
+if ((a == 6) || (a == 7))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    if ((a > 0) && (a < 6))
+    {
+        Console.WriteLine("No");
+    }
+    else
+    {
+        Console.WriteLine("Every week have 7 days! Started from 1 to 7   :) ");
+    }
+}
