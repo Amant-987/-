39c4bf8a5c39694fa2d044a795ee4c98f2ab4dc4
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-27 14:28:36 +0300

    task done

--- C_Sharp/Seminar_2/task_4/Program.cs
+++ C_Sharp/Seminar_2/task_4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 16 Напишите программу, которая принимает на вход два числа 
+//и проверяет,
+// является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.Clear();
+Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+
+if (Math.Pow(a,2) == b || Math.Pow(b,2) == a )
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("NO");
+}
