6b57c8681bdea8d3837d326c4cd4a42402c5bd63
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-28 16:47:41 +0300

    HM_6 done

---
+++ C_Sharp/Seminar_2_HM/HM_6/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа
+или сообщает что третьей цифры нет.
+645 -> 5
+78 -> didn't have third-digit
+32679 -> 6
+*/
+Console.Clear();
+Console.Write("Enter number: ");
+int a = int.Parse(Console.ReadLine());
+ if (a < 100)
+ {
+    Console.Write("Didn't have third digit!");
+ }
+ else
+ { while (a > 999)
+   {
+   a /= 10;
+   }
+   Console.Write($"{a % 10}");
+ }
