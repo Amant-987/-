8f75d803dfe856e8a6ad16ab230ae061c68d30b3
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-30 13:36:06 +0300

    new comand switch

---
+++ C_Sharp/Seminar_3/task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// **Задача 18:**
+
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+class task_1
+{
+public static void Main()
+{
+    Console.Clear();
+    Console.Write("Enter number of quadra: ");
+    int quad = int.Parse(Console.ReadLine());
+
+switch (quad)
+{
+    case 1:
+    Console.Write("x>0, y>0");
+    break;
+    case 2:
+    Console.Write("x<0, y>0");
+    break;
+    case 3:
+    Console.Write("x<0, y<0");
+    break;
+    case 4:
+    Console.Write("x>0, y<0");
+    break;
+    default:{
+    Console.Write("Quadra didn't have number like yours!");
+     break;
+    }
+}
+
+}
+
+
+}
