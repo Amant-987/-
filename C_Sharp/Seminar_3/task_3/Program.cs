b244cc2f5f91a7a47217b15f4049ec47fa268481
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-30 14:13:23 +0300

    for+math.pow

---
+++ C_Sharp/Seminar_3/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+for (int i=1; i < num + 1; i++)
+{
+    if(i!=num)
+{
+    Console.Write(Math.Pow(i,2)+", ");
+        }
+    else
+    {
+        
+    Console.Write(Math.Pow(i,2));
+    }
+
+}
