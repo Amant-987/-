e9269bdecde0d05bfa7fb722c16c54472a9d5070
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-30 12:52:20 +0300

    for check

---
+++ C_Sharp/Testing_Skill/check/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 || number < 1000)
+{
+int result = ((number % 100 - number % 10) / 10);
+Console.WriteLine(result);
+}
+else
+Console.WriteLine("Это не трехзначное число: ");
