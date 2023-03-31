6e56df7057534bd0eeb5e0f120dde0929b5d5610
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-31 23:26:13 +0300

    hm_9 done

--- C_Sharp/Seminar_3_HM/HM_9/Program.cs
+++ C_Sharp/Seminar_3_HM/HM_9/Program.cs
@@ -2,3 +2,19 @@
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.Write("Enter X-coordinate A: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Enter Y-coordinate A: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Enter Z-coordinate A: ");
+double z1 = double.Parse(Console.ReadLine());
+Console.Write("Enter X-coordinate B: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Enter Y-coordinate B: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Enter Z-coordinate B: ");
+double z2 = double.Parse(Console.ReadLine());
+
+Console.Write(Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)+Math.Pow((z1-z2),2)));
