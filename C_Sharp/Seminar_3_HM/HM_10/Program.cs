5d8b42cd3c194eb7176048c9dc46282e50023226
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-03-31 23:48:10 +0300

    hm_10 done

--- C_Sharp/Seminar_3_HM/HM_10/Program.cs
+++ C_Sharp/Seminar_3_HM/HM_10/Program.cs
@@ -2,3 +2,20 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // // 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+for (int i=1; i < num + 1; i++)
+{
+    if(i!=num)
+{
+    Console.Write(Math.Pow(i,3)+", ");
+        }
+    else
+    {
+        
+    Console.Write(Math.Pow(i,3));
+    }
+
+}
