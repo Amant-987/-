690b6580175fe3fd3ed782e77c277be054574315
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-05 12:39:43 +0300

    new example of task

--- C_Sharp/Seminar_4_HM/HM_11/Program.cs
+++ C_Sharp/Seminar_4_HM/HM_11/Program.cs
@@ -4,30 +4,15 @@
 
 // 2, 4 -> 16
 
-// int Degree(double num, double num2)
-// {        double result = 0;
-//     result = Math.Pow(num,num2);
-// return result;
-// }
-
-// Console.Clear();
-// Console.WriteLine("Enter number: ");
-// int num = int.Parse(Console.ReadLine());
-// Console.WriteLine("Enter degree: ");
-// int num2 = int.Parse(Console.ReadLine());
-// Console.Write($"The number {num} to the degree of {num} is {Degree}");
-
-
-int Degree(int num, int grad){
-  int result = 1;
-  for(int i=1; i <= grad; i++){
-    result = result * num;
-  }
-    return result;
+double Degree(double num, double num2)
+{        double result = 0;
+    result = Math.Pow(num,num2);
+return result;
 }
-  Console.Write("Number: ");
-  int num = int.Parse(Console.ReadLine());
-  Console.Write("Degree: ");
-  int grad = int.Parse(Console.ReadLine());
-  int degree = Degree(num, grad);
-  Console.WriteLine("Result: " + degree);
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter degree: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write($"{num} ^ {num2} = {Degree(num,num2)}");
