83c50b979b98ef693e6039fbbbd332eff6e316c7
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-05 11:42:22 +0300

    replace wrong code

---
+++ C_Sharp/Seminar_4/task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов
+// заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+// int a= new Random().Next(2,7)
+// int b = new Random().Next(2)
+
+int[] BinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 1; i < size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
+Console.Write("Enter number: ");
+int size = int.Parse(Console.ReadLine());
+//Console.Write($"[{String.Join(", ", BinaryArray)}]");
+Console.WriteLine($"[{String.Join(", ", BinaryArray(size))}]");
