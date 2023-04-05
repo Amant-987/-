ffa4dfcbd14143b5186f5cf96412afe5de6503f1
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-05 11:57:52 +0300

    hm_13 done

---
+++ C_Sharp/Seminar_4_HM/HM_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int a = 0; a < size; a++)
+    {
+        result[a] = new Random().Next(100);
+    }
+    return result;
+}
+
+int size = 18;
+Console.WriteLine($"[{String.Join(" | ", Array(size))}]");
