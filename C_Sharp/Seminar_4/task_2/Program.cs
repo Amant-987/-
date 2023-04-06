9c2f67201389ec06460a41951393d53b9951c0f0
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-06 13:18:20 +0300

    foreach

--- C_Sharp/Seminar_4/task_2/Program.cs
+++ C_Sharp/Seminar_4/task_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая принимает на вход число N 
+// // и выдает произведение чисел от 1 до N 
+// 4->24
+// 5-> 120 == 1*2*3*4*5
+
+int multiply(int num)
+{
+    int result = 1;
+    for (int i = 2; i < num + 1; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.Write(multiply(num));
---
+++ C_Sharp/Seminar_5/Task_0/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] SumPosNeg(int[] array)
+{
+    int[] result = new int[2];
+    foreach (int el in array)//[1, 2, 3, 4, -5, 1, -3] el = -5
+    {
+        //int a = el > 0 ? el : 0;
+        //result[0] += a;
+        result[0] += el > 0 ? el : 0;//ps += -5 > 0? 2 : 0;
+        result[1] += el < 0 ? el : 0;
+    }
+    return result;
+}
