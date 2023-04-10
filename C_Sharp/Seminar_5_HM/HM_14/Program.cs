102291fd03f54d268b42a3b77e382d3621af35b6
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-10 12:43:23 +0300

    new version hm14

--- C_Sharp/Seminar_5_HM/HM_14/Program.cs
+++ C_Sharp/Seminar_5_HM/HM_14/Program.cs
@@ -4,20 +4,32 @@ int[] array = GetArray(10, 100, 999);
 int count = 0;
 Console.WriteLine(string.Join(", ", array));
 Console.WriteLine($" -> Count even num: {Evennum(count)}");
-int[] GetArray(int size, int minvalue, int maxvalue)
+int[] GetArray(int size, int minv, int maxv)
 {
-    int[] res = new int[size];
+    int[] a = new int[size];
     for (int i = 0; i < size; i++)
     {
-        res[i] = new Random().Next(minvalue, maxvalue + 1);
+        a[i] = new Random().Next(minv, maxv + 1);
     }
-    return res;
+    return a;
 }
+// int Evennum(int count)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 int Evennum(int count)
 {
-    for (int i = 0; i < array.Length; i++)
+    int i = 0;
+    foreach (var item in array)
     {
-        if (array[i] % 2 == 0)
+        if (item % 2 == 0)
         {
             count++;
         }
