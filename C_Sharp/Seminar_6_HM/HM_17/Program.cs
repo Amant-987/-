9efca309389961c5866815843a0d15717e3e3232
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-13 00:11:13 +0300

    HM_17 done

---
+++ C_Sharp/Seminar_6_HM/HM_17/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Enter N numbers with space between it: ");
+string[] array = Console.ReadLine().Split(' ');
+
+int Count(string[] array)
+{
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (int.Parse(array[i]) > 0) c++;
+    }
+
+    return c;
+}
+Count(array);
+
+Console.WriteLine(String.Join(" / ", array));
+Console.WriteLine(Count(array));
