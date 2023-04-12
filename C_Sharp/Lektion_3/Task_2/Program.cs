02264f7b9ac2fa4d90671142510ebe0587b0620c
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-12 23:07:04 +0300

    method and for and void

--- C_Sharp/Lektion_3/Task_2/Program.cs
+++ C_Sharp/Lektion_3/Task_2/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+//Method 3 input = something, output = result
+// string Method4(int count, string text)
+// {
+//     int i =0;
+//     string result = string.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf, ");
+Console.WriteLine(res);
