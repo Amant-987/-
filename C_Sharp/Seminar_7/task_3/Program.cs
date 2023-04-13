8e7fd8c38ea33dc99276069fdb05c36cfa3b94e0
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-13 14:17:53 +0300

    seminar 7

--- C_Sharp/Seminar_7/task_3/Program.cs
+++ C_Sharp/Seminar_7/task_3/Program.cs
@@ -1,2 +1,132 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **Задача 49:** Задайте двумерный массив.
+//  Найдите элементы, у которых оба индекса нечётные,
+//   и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 2 3 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+// 1 **4** 3 **16**
+
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите количество строк массива (m): ");
+int rows = int.Parse(ReadLine()!);
+
+Write("Введите количество столбцов массива (n): ");
+int columns = int.Parse(ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+int[,] NewArray = ChangeArray(array);
+PrintArray(NewArray);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int[,] ChangeArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        if (i % 2 == 1)
+        {
+            for (int j = 0; j < inArray.GetLength(1); j++)
+            {
+                if (j % 2 == 1)
+                {
+                    inArray[i, j] = inArray[i, j] * inArray[i, j];
+                }
+            }
+        }
+    }
+    return inArray;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+
+// Clear();
+
+// Write("Введите количество строк массива: ");
+// int rows = int.Parse(ReadLine());
+
+// Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(ReadLine());
+
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+// WriteLine();
+// ChangeArray(array);
+// PrintArray(array);
+// WriteLine();
+// PrintArray(ChangeArray(array));
+// WriteLine();
+// PrintArray(array);
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+
+//     }
+//     return result;
+// }
+
+// int[,] ChangeArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (i % 2 == 1 && j % 2 == 1)
+//             {
+//                 array[i, j] *= array[i, j];
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Write($"{array[i, j]} ");
+//         }
+//         WriteLine();
+//     }
+// }
---
+++ C_Sharp/Seminar_7/task_4/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся
+//  на главной диагонали (с индексами (0,0); (1; 1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+WriteLine();
+SumArray(array);
+/*
+int GetSumDiag(int[,] array)
+{
+    int length = array.GetLength(0) < array.GetLength(1) ? array.GetLength(0) : array.GetLength(1);
+    // if (array.GetLength(0) < array.GetLength(1))
+    //     length = array.GetLength(0);
+    // else
+    //     length = array.GetLength(1);
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i, i];
+    }
+    return sum;
+}
+*/
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void SumArray(int[,] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (j == i)
+            {
+                count = count + inArray[i, j];
+            }
+        }
+    }
+    WriteLine(count);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
