bebdbea8ee51cfac71de6f55f6417b12f7d39b78
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-20 14:25:11 +0300

    seminar 9 task 2+3

---
+++ C_Sharp/Seminar_9/task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter your number: ");
+int a = int.Parse(ReadLine());
+Write(SumNumbers(a));
+
+// int sum = 0;
+// while(a > 0){
+//     sum += a % 10;
+//     a /= 10;
+// }
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+        return 0;
+    return (number % 10 + SumNumbers(number / 10));
+}
---
+++ C_Sharp/Seminar_9/task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, 
+//и возводит число А в целую степень B с помощью рекурсии.
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter your number: ");
+int number = int.Parse(ReadLine()!);
+Write("Enter your pow: ");
+int pow = int.Parse(ReadLine()!);
+WriteLine($"{PowNumbers(number, pow)}");
+
+// int sum = 0;
+// while(a > 0){
+//     sum += a % 10;
+//     a /= 10;
+// }
+
+int PowNumbers(int number, int pow)
+{
+    if (pow == 0)
+        return 1;
+    return PowNumbers(number, --pow) * number;
+}
