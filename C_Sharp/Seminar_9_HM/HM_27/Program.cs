9c7db3949bfe11f10d21ce7c9c8734fe49873e8a
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-24 13:12:31 +0300

    hm_27 done

---
+++ C_Sharp/Seminar_9_HM/HM_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+
+int n = new Random().Next(3,20);
+int m = 1;
+
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
