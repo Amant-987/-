82be1afafb0347e9523c58672a6fbb3483c9f43a
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-04-24 14:10:41 +0300

    HM_29 done

---
+++ C_Sharp/Seminar_9_HM/HM_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления 
+// функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+using System;
+using static System.Console;
+
+int Num = new Random().Next(1, 3);
+int Num2 = new Random().Next(1, 4);
+
+WriteLine($"Function of Akkerman for number {Num} and {Num2} = {Akkerman(Num, Num2)}");
+
+int Akkerman(int i, int n)
+{
+    if (i == 0)
+        return n + 1;
+    if (i > 0 && n == 0)
+        return Akkerman(i - 1, 1);
+    else
+        return Akkerman(i - 1, Akkerman(i, n - 1));
+}
