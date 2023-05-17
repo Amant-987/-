22db7f128b23e628e6b053dffa77ec69795a2246
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-05-17 14:25:55 +0300

    check

--- bootcamp/task_1/Program.cs
+++ bootcamp/task_1/Program.cs
@@ -1,48 +1,104 @@
-﻿/*
-1. Константные(+)
-2. Логарифмические(бинарный поиск) O(log2(n)) (+)
-3. Линейные(+)
-4. Линейно-логарифмические(быстрая сортировка, сортировка слиянием) (+)
-5. Квадратные(сортировак пузырьком, сортировка выбором, сортировка вставками)
-6. Кубические
-7. Степенные (рекурсия)
-*/
+﻿// /*
+// 1. Константные(+)
+// 2. Логарифмические(бинарный поиск) O(log2(n)) (+)
+// 3. Линейные(+)
+// 4. Линейно-логарифмические(быстрая сортировка, сортировка слиянием) (+)
+// 5. Квадратные(сортировак пузырьком, сортировка выбором, сортировка вставками)
+// 6. Кубические
+// 7. Степенные (рекурсия)
+// */
 
 
-// Напишите программу, которая запрашивает у пользователя число(n). Необходимо сумму чисел от 1 до n.
-// Console.Clear();
-// Console.Write("Введите число: ");
-// int n = int.Parse(Console.ReadLine()!);
-// Console.WriteLine($"Результат: {((n + 1) / 2.0) * n}");
-// O(1)
+// // Напишите программу, которая запрашивает у пользователя число(n). Необходимо сумму чисел от 1 до n.
+// // Console.Clear();
+// // Console.Write("Введите число: ");
+// // int n = int.Parse(Console.ReadLine()!);
+// // Console.WriteLine($"Результат: {((n + 1) / 2.0) * n}");
+// // O(1)
 
-// int n = int.Parse(Console.ReadLine()!);
-// int n = Convert.ToInt32(Console.ReadLine());
-// int result = 0;
-// for (int i = 1; i <= n; i++)
-//     result = result + i;
-// Console.WriteLine($"Результат: {result}");
-// O(n)
+// // int n = int.Parse(Console.ReadLine()!);
+// // int n = Convert.ToInt32(Console.ReadLine());
+// // int result = 0;
+// // for (int i = 1; i <= n; i++)
+// //     result = result + i;
+// // Console.WriteLine($"Результат: {result}");
+// // O(n)
 
+
+// // void InputArray(int[] array)
+// // {
+// //     for (int i = 0; i < array.Length; i++)
+// //         array[i] = new Random().Next(1, 101); // [1, 100]
+// // }
+
+// // void SortArray(int[] array)
+// // {
+// //     for (int i = 0; i < array.Length; i++)
+// //     {
+// //         for (int j = 0; j < array.Length - 1; j++)
+// //         {
+// //             if (array[j] > array[j + 1])
+// //             {
+// //                 int temp = array[j];
+// //                 array[j] = array[j + 1];
+// //                 array[j + 1] = temp;
+// //             }
+// //         }
+// //     }
+// // }
+
+// // Console.Clear();
+// // Console.Write("Введите кол-во элементов массива: ");
+// // int n = int.Parse(Console.ReadLine()!);
+// // int[] array = new int[n];
+// // InputArray(array);
+// // Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// // SortArray(array);
+// // Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+// // O(n^2)
+
+// int[] array = new int[5];
+// int[,] array2D = new int[5, 5];
+// int[,,] array3D = new int[5, 5, 5];
+// int count = 0;
+// for (int i = 0; i < array3D.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array3D.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < array3D.GetLength(2); k++)
+//         {
+//             array3D[i, j, k] = count;
+//             count++;
+//             Console.WriteLine($"{array3D[i, j, k]} ({i}, {j}, {k})");
+//         }
+//     }
+// }
+// // O(n^3)
 
 // void InputArray(int[] array)
 // {
 //     for (int i = 0; i < array.Length; i++)
-//         array[i] = new Random().Next(1, 101); // [1, 100]
+//         array[i] = new Random().Next(1, 101); // [1; 100]
 // }
 
-// void SortArray(int[] array)
+
+// void ComptetionArray(int[] array, int[] HelpArray)
 // {
 //     for (int i = 0; i < array.Length; i++)
+//         HelpArray[array[i]] += 1;
+// }
+
+
+// void SortArray(int[] array, int[] HelpArray)
+// {
+//     int j = 0;
+//     for (int i = 1; i <= 100; i++)
 //     {
-//         for (int j = 0; j < array.Length - 1; j++)
+//         while (HelpArray[i] > 0)
 //         {
-//             if (array[j] > array[j + 1])
-//             {
-//                 int temp = array[j];
-//                 array[j] = array[j + 1];
-//                 array[j + 1] = temp;
-//             }
+//             array[j] = i;
+//             j++;
+//             HelpArray[i]--;
 //         }
 //     }
 // }
@@ -50,138 +106,82 @@
 // Console.Clear();
 // Console.Write("Введите кол-во элементов массива: ");
 // int n = int.Parse(Console.ReadLine()!);
-// int[] array = new int[n];
-// InputArray(array);
-// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
-// SortArray(array);
-// Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
-// O(n^2)
-
-int[] array = new int[5];
-int[,] array2D = new int[5, 5];
-int[,,] array3D = new int[5, 5, 5];
-int count = 0;
-for (int i = 0; i < array3D.GetLength(0); i++)
-{
-    for (int j = 0; j < array3D.GetLength(1); j++)
-    {
-        for (int k = 0; k < array3D.GetLength(2); k++)
-        {
-            array3D[i, j, k] = count;
-            count++;
-            Console.WriteLine($"{array3D[i, j, k]} ({i}, {j}, {k})");
-        }
-    }
-}
-// O(n^3)
-
-void InputArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(1, 101); // [1; 100]
-}
+// // int[] array = new int[n];
+// // int[] HelpArray = new int[101];
+// // InputArray(array);
+// // Console.WriteLine($"[{string.Join(", ", array)}]");
+// // ComptetionArray(array, HelpArray);
+// // SortArray(array, HelpArray);
+// // Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
 
 
-void ComptetionArray(int[] array, int[] HelpArray)
-{
-    for (int i = 0; i < array.Length; i++)
-        HelpArray[array[i]] += 1;
-}
+// /*
+// QuickSort(Быстрая Сортировка)
+// O(n * log2(n))
+// */
+// // [5, 8, 23, 4, 2, 1, 10]
+// // [5, 8, 23, 10]
+// // [4, 2, 1]
+// // void InputArray(int[] array)
+// // {
+// //     for (int i = 0; i < array.Length; i++)
+// //         array[i] = new Random().Next(-100, 101); // [-100; 100]
+// // }
 
 
-void SortArray(int[] array, int[] HelpArray)
-{
-    int j = 0;
-    for (int i = 1; i <= 100; i++)
-    {
-        while (HelpArray[i] > 0)
-        {
-            array[j] = i;
-            j++;
-            HelpArray[i]--;
-        }
-    }
-}
-
-Console.Clear();
-Console.Write("Введите кол-во элементов массива: ");
-int n = int.Parse(Console.ReadLine()!);
-// int[] array = new int[n];
-// int[] HelpArray = new int[101];
-// InputArray(array);
-// Console.WriteLine($"[{string.Join(", ", array)}]");
-// ComptetionArray(array, HelpArray);
-// SortArray(array, HelpArray);
-// Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
+// // int SortQuick(int[] array, int start, int end)
+// // {
+// //     int temp;
+// //     int marker = start;
+// //     for (int i = start; i < end; i++)
+// //     {
+// //         if (array[i] < array[end])
+// //         {
+// //             temp = array[marker];
+// //             array[marker] = array[i];
+// //             array[i] = temp;
+// //             marker += 1;
+// //         }
+// //     }
+// //     temp = array[marker];
+// //     array[marker] = array[end];
+// //     array[end] = temp;
+// //     return marker;
+// // }
 
 
-/*
-QuickSort(Быстрая Сортировка)
-O(n * log2(n))
-*/
-// [5, 8, 23, 4, 2, 1, 10]
-// [5, 8, 23, 10]
-// [4, 2, 1]
-// void InputArray(int[] array)
+// // void recSsort(int[] array, int start, int end, int count)
+// // {
+// //     count++;
+// //     Console.WriteLine($"[{string.Join(", ", array)}]");
+// //     Console.WriteLine($"Количество {count} нач. {start} кон. {end}");
+// //     if (start >= end)
+// //         return ;
+// //     int pivot = SortQuick(array, start, end);
+// //     recSsort(array, start, pivot - 1, count);
+// //     recSsort(array, pivot + 1, end, count);   
+// // }
+
+
+// // Console.Clear();
+// // Console.Write("Введите кол-во элементов массива: ");
+// // int n = int.Parse(Console.ReadLine()!);
+// // int[] array = new int[n];
+// // InputArray(array);
+// // Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// // // Метод флажка <-> return
+// // recSsort(array, 0, array.Length - 1, 0);
+// // Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
+
+// // Метод флажка
+// int i = 1;
+// bool flag = true;
+// while (i < 2019 && flag)
 // {
-//     for (int i = 0; i < array.Length; i++)
-//         array[i] = new Random().Next(-100, 101); // [-100; 100]
-// }
-
-
-// int SortQuick(int[] array, int start, int end)
-// {
-//     int temp;
-//     int marker = start;
-//     for (int i = start; i < end; i++)
+//     if (i % 573 == 0)
 //     {
-//         if (array[i] < array[end])
-//         {
-//             temp = array[marker];
-//             array[marker] = array[i];
-//             array[i] = temp;
-//             marker += 1;
-//         }
+//         Console.WriteLine(i);
+//         flag = false;
 //     }
-//     temp = array[marker];
-//     array[marker] = array[end];
-//     array[end] = temp;
-//     return marker;
+//     i++;
 // }
-
-
-// void recSsort(int[] array, int start, int end, int count)
-// {
-//     count++;
-//     Console.WriteLine($"[{string.Join(", ", array)}]");
-//     Console.WriteLine($"Количество {count} нач. {start} кон. {end}");
-//     if (start >= end)
-//         return ;
-//     int pivot = SortQuick(array, start, end);
-//     recSsort(array, start, pivot - 1, count);
-//     recSsort(array, pivot + 1, end, count);   
-// }
-
-
-// Console.Clear();
-// Console.Write("Введите кол-во элементов массива: ");
-// int n = int.Parse(Console.ReadLine()!);
-// int[] array = new int[n];
-// InputArray(array);
-// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
-// // Метод флажка <-> return
-// recSsort(array, 0, array.Length - 1, 0);
-// Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
-
-// Метод флажка
-int i = 1;
-bool flag = true;
-while (i < 2019 && flag)
-{
-    if (i % 573 == 0)
-    {
-        Console.WriteLine(i);
-        flag = false;
-    }
-    i++;
-}
